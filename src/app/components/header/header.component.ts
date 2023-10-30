dfd7e50d9bc23d1fc74c515f497648972db11049
juan morales <juanskate326@gmail.com>
2023-10-30 17:07:30 -0500

    chat

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,242 +1,250 @@
 import {
-	AfterViewInit,
-	Component,
-	ElementRef,
-	EventEmitter,
-	Injectable,
-	OnInit,
-	Output,
-	ViewChild,
-} from "@angular/core"
-import { Location } from "@angular/common"
-import { EPages, Usuario } from "src/app/tools/models"
-import { Router } from "@angular/router"
-import { Store } from "@ngrx/store"
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Injectable,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Location } from '@angular/common';
+import { EPages, Usuario } from 'src/app/tools/models';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 import {
-	userSelect,
-	matchPendingSelect,
-	notificationSelect,
-	matchSelect,
-	messagesSelect,
-} from "src/redux/selectors"
-import Swal from "sweetalert2"
-import { MatchService } from "src/app/services/match/match.service"
+  userSelect,
+  matchPendingSelect,
+  notificationSelect,
+  matchSelect,
+  messagesSelect,
+} from 'src/redux/selectors';
+import Swal from 'sweetalert2';
+import { MatchService } from 'src/app/services/match/match.service';
 import {
-	myMatches,
-	newNotification,
-	newPendingMatch,
-	userChat,
-} from "src/redux/actions"
-import { NotifyService } from "src/app/services/notify/notify.service"
+  myMatches,
+  newNotification,
+  newPendingMatch,
+  userChat,
+} from 'src/redux/actions';
+import { NotifyService } from 'src/app/services/notify/notify.service';
+import { ChatService } from 'src/app/services/chat/chat.service';
 
 @Component({
-	selector: "app-header",
-	templateUrl: "./header.component.html",
-	styleUrls: ["./header.component.scss"],
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-	page: EPages = EPages.landing
-	user$ = this.store.select(userSelect)
-	matchsRequest$ = this.store.select(matchPendingSelect)
-	misMatches$ = this.store.select(matchSelect)
-	misMatchesChat$ = this.store.select(matchSelect)
-	notification$ = this.store.select(notificationSelect)
-	messages$ = this.store.select(messagesSelect)
-	userChat!: Usuario | null
-	isCloseChat = true
-	isOpenSideBarChat = false
-	notificaciones: any[] = []
-	misMensajesNoVistos: any = []
-	@Output() event = new EventEmitter()
-	@ViewChild("parentElement") parentElementRef!: ElementRef
-	showChild: boolean[] = []
-	showCollapse(index: number) {
-		this.showChild[index] = true
-	}
-	hideCollapse(index: number) {
-		this.showChild[index] = false
-	}
-	constructor(
-		private locate: Location,
-		private route: Router,
-		private matchSrvc: MatchService,
-		private notifySrvc: NotifyService,
-		private store: Store<any>,
-	) {
-		this.locate.onUrlChange(() => {
-			switch (this.locate.path()) {
-				case "/home":
-					this.page = EPages.home
-					break
-				case "/login":
-					this.page = EPages.login
-					break
-				case "":
-					this.page = EPages.landing
-					break
-				default:
-					this.page = EPages.home
-					break
-			}
-		})
-	}
-	ngOnInit(): void {
-		this.notification$.subscribe((data: any) => {
-			this.notificaciones = data
-		})
-		this.messages$.subscribe((data: any) => {
-			this.misMensajesNoVistos = data.filter(
-				(item: any) => item.estado === "no_visto",
-			)
-		})
-	}
+  page: EPages = EPages.landing;
+  user$ = this.store.select(userSelect);
+  matchsRequest$ = this.store.select(matchPendingSelect);
+  misMatches$ = this.store.select(matchSelect);
+  misMatchesChat: any[] = [];
+  notification$ = this.store.select(notificationSelect);
+  messages$ = this.store.select(messagesSelect);
+  userChat!: Usuario | null;
+  isCloseChat = true;
+  isOpenSideBarChat = false;
+  notificaciones: any[] = [];
+  misMensajesNoVistos: any = [];
+  @Output() event = new EventEmitter();
+  @ViewChild('parentElement') parentElementRef!: ElementRef;
+  showChild: boolean[] = [];
+  showCollapse(index: number) {
+    this.showChild[index] = true;
+  }
+  hideCollapse(index: number) {
+    this.showChild[index] = false;
+  }
+  constructor(
+    private locate: Location,
+    private route: Router,
+    private matchSrvc: MatchService,
+    private notifySrvc: NotifyService,
+    private chatSrvc: ChatService,
+    private store: Store<any>
+  ) {
+    this.locate.onUrlChange(() => {
+      switch (this.locate.path()) {
+        case '/home':
+          this.page = EPages.home;
+          break;
+        case '/login':
+          this.page = EPages.login;
+          break;
+        case '':
+          this.page = EPages.landing;
+          break;
+        default:
+          this.page = EPages.home;
+          break;
+      }
+    });
+  }
+  ngOnInit(): void {
+    this.notification$.subscribe((data: any) => {
+      this.notificaciones = data;
+    });
+    this.misMatches$.subscribe((data) => {
+      this.misMatchesChat = data;
+      console.log(data);
+    });
+    this.messages$.subscribe((data: any) => {
+      console.log(data);
+      this.misMensajesNoVistos = data.filter(
+        (item: any) => item.estado === 'no_visto'
+      );
+      this.organizarChats(data);
+    });
+  }
 
-	calcularTiempoTranscurrido(desde: string): string {
-		const fechaActual = new Date()
-		const fechaPasada = new Date(desde)
-		const diferenciaMilisegundos = fechaActual.getTime() - fechaPasada.getTime()
+  calcularTiempoTranscurrido(desde: string): string {
+    const fechaActual = new Date();
+    const fechaPasada = new Date(desde);
+    const diferenciaMilisegundos =
+      fechaActual.getTime() - fechaPasada.getTime();
 
-		// Cálculo de las diferentes unidades de tiempo
-		const segundos = Math.floor(diferenciaMilisegundos / 1000)
-		const minutos = Math.floor(segundos / 60)
-		const horas = Math.floor(minutos / 60)
-		const dias = Math.floor(horas / 24)
+    // Cálculo de las diferentes unidades de tiempo
+    const segundos = Math.floor(diferenciaMilisegundos / 1000);
+    const minutos = Math.floor(segundos / 60);
+    const horas = Math.floor(minutos / 60);
+    const dias = Math.floor(horas / 24);
 
-		if (dias > 0) {
-			return `Hace ${dias} día(s)`
-		} else if (horas > 0) {
-			return `Hace ${horas} hora(s)`
-		} else if (minutos > 0) {
-			return `Hace ${minutos} minuto(s)`
-		} else {
-			return `Hace unos segundos`
-		}
-	}
+    if (dias > 0) {
+      return `Hace ${dias} día(s)`;
+    } else if (horas > 0) {
+      return `Hace ${horas} hora(s)`;
+    } else if (minutos > 0) {
+      return `Hace ${minutos} minuto(s)`;
+    } else {
+      return `Hace unos segundos`;
+    }
+  }
 
-	async aceptar(user: Usuario) {
-		Swal.fire({
-			title: "¿Seguro quieres aceptar la solicitud?",
-			showCancelButton: true,
-			confirmButtonText: "Aceptar",
-			cancelButtonText: "Cancelar",
-			confirmButtonColor: "#28a745",
-		}).then(async (result) => {
-			if (result.isConfirmed) {
-				const currentUser = JSON.parse(localStorage.getItem("user")!)
-				const body = {
-					idToMatch: user.id,
-					idUser: currentUser.id,
-				}
-				const res = await this.matchSrvc.createMatch(body)
-				res.subscribe(
-					(data) => {
-						this.store.dispatch(myMatches.set(user))
-						this.store.dispatch(newPendingMatch.delete(user))
-						Swal.fire("¡Solicitud aceptada!", "", "success")
-						console.log(data)
-						this.event.emit("match")
-					},
-					(err) => {
-						Swal.fire("Error", err, "error")
-						console.log("error::", err)
-					},
-				)
-			}
-		})
-	}
-	async eliminarSolicitud(user: Usuario) {
-		Swal.fire({
-			title: "¿Seguro quieres cancelar la solicitud?",
-			showCancelButton: true,
-			confirmButtonText: "Aceptar",
-			cancelButtonText: "Cancelar",
-			icon: "warning",
-		}).then(async (result) => {
-			if (result.isConfirmed) {
-				const res = await this.matchSrvc.rejectPendingMatch(user.id.toString())
-				res.subscribe(
-					(data) => {
-						Swal.fire("¡Solicitud eliminada!", "", "success")
-						this.store.dispatch(newPendingMatch.delete(user))
-						this.event.emit({ type: "deleteRequest", user: user })
-					},
-					(err) => {
-						console.log("error::", err)
-					},
-				)
-			}
-		})
-	}
+  async aceptar(user: Usuario) {
+    Swal.fire({
+      title: '¿Seguro quieres aceptar la solicitud?',
+      showCancelButton: true,
+      confirmButtonText: 'Aceptar',
+      cancelButtonText: 'Cancelar',
+      confirmButtonColor: '#28a745',
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const currentUser = JSON.parse(localStorage.getItem('user')!);
+        const body = {
+          idToMatch: user.id,
+          idUser: currentUser.id,
+        };
+        const res = await this.matchSrvc.createMatch(body);
+        res.subscribe(
+          (data) => {
+            this.store.dispatch(myMatches.set(user));
+            this.store.dispatch(newPendingMatch.delete(user));
+            Swal.fire('¡Solicitud aceptada!', '', 'success');
+            console.log(data);
+            this.event.emit('match');
+          },
+          (err) => {
+            Swal.fire('Error', err, 'error');
+            console.log('error::', err);
+          }
+        );
+      }
+    });
+  }
+  async eliminarSolicitud(user: Usuario) {
+    Swal.fire({
+      title: '¿Seguro quieres cancelar la solicitud?',
+      showCancelButton: true,
+      confirmButtonText: 'Aceptar',
+      cancelButtonText: 'Cancelar',
+      icon: 'warning',
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await this.matchSrvc.rejectPendingMatch(user.id.toString());
+        res.subscribe(
+          (data) => {
+            Swal.fire('¡Solicitud eliminada!', '', 'success');
+            this.store.dispatch(newPendingMatch.delete(user));
+            this.event.emit({ type: 'deleteRequest', user: user });
+          },
+          (err) => {
+            console.log('error::', err);
+          }
+        );
+      }
+    });
+  }
 
-	openSideBarChat() {
-		this.isOpenSideBarChat = !this.isOpenSideBarChat
-	}
+  openSideBarChat() {
+    this.isOpenSideBarChat = !this.isOpenSideBarChat;
+  }
 
-	setUserChat(user: Usuario) {
-		this.store.dispatch(userChat.set(user))
-	}
-	quitUserChat() {
-		console.log("quitUser")
-		this.userChat = null
-	}
-	verPerfil(user: Usuario) {
-		this.route.navigate([`/user/${user.id}`])
-	}
-	logOut() {
-		localStorage.removeItem("user")
-		localStorage.removeItem("token")
-		location.reload()
-	}
+  setUserChat(user: Usuario) {
+    this.store.dispatch(userChat.set(user));
+  }
 
-	async eliminarNotificaciones() {
-		this.notificaciones.forEach(async (notificacion: any) => {
-			const res = await this.notifySrvc.deleteNotify(notificacion.id)
-			res.subscribe(
-				(data) => {
-					console.log(data)
-					this.store.dispatch(newNotification.delete(notificacion))
-				},
-				(err) => {
-					console.log("error::", err)
-				},
-			)
-		})
-	}
-	async eliminarNotificacion(notificacion: any) {
-		const res = await this.notifySrvc.deleteNotify(notificacion.id)
-		res.subscribe(
-			(data) => {
-				console.log(data)
-				this.store.dispatch(newNotification.delete(notificacion))
-			},
-			(err) => {
-				console.log("error::", err)
-			},
-		)
-	}
-	getMessagesNoLeidos(userId: number): Number {
-		return this.misMensajesNoVistos.filter(
-			(item: any) => item.remitente_id === userId,
-		).length
-	}
-	closeSidebarLeft() {
-		//remove the class lg-menu-open
-		$("#sidebar-left").removeClass("lg-menu-open")
-		$("#wrapper").removeClass("open")
-	}
-obtenerIniciales(nombreCompleto: string): string {
-  // Dividir el nombre completo en palabras
-  const palabras = nombreCompleto.split(" ");
-  
-  // Obtener la primera letra de cada palabra
-  const iniciales = palabras.map(palabra => palabra.charAt(0));
-  
-  // Tomar las dos primeras iniciales (nombre y primer apellido)
-  const inicialesNombreApellido = iniciales.slice(0, 2);
-  
-  // Unir las iniciales en una cadena
-  const inicialesString = inicialesNombreApellido.join("");
-  
-  return inicialesString.toUpperCase()
-}
+  quitUserChat() {
+    console.log('quitUser');
+    this.userChat = null;
+  }
+
+  verPerfil(user: Usuario) {
+    this.route.navigate([`/user/${user.id}`]);
+  }
+
+  logOut() {
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    location.reload();
+  }
+
+  async eliminarNotificaciones() {
+    this.notificaciones.forEach(async (notificacion: any) => {
+      const res = await this.notifySrvc.deleteNotify(notificacion.id);
+      res.subscribe(
+        (data) => {
+          console.log(data);
+          this.store.dispatch(newNotification.delete(notificacion));
+        },
+        (err) => {
+          console.log('error::', err);
+        }
+      );
+    });
+  }
+
+  async eliminarNotificacion(notificacion: any) {
+    const res = await this.notifySrvc.deleteNotify(notificacion.id);
+    res.subscribe(
+      (data) => {
+        console.log(data);
+        this.store.dispatch(newNotification.delete(notificacion));
+      },
+      (err) => {
+        console.log('error::', err);
+      }
+    );
+  }
+
+  getMessagesNoLeidos(userId: number): Number {
+    return this.misMensajesNoVistos.filter(
+      (item: any) => item.remitente_id === userId
+    ).length;
+  }
+
+  closeSidebarLeft() {
+    //remove the class lg-menu-open
+    $('#sidebar-left').removeClass('lg-menu-open');
+    $('#wrapper').removeClass('open');
+  }
+
+  organizarChats(data: any[]) {
+    // this.misMatchesChat.sort((a: any, b: any) => {
+    //   return (
+    //     new Date(b.fecha_envio).getTime() - new Date(a.fecha_envio).getTime()
+    //   );
+    // });
+  }
 }
--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -1,46 +1,56 @@
-import { HttpClient } from "@angular/common/http"
-import { Injectable } from "@angular/core"
-import { environment } from "src/environments/environment"
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-	providedIn: "root",
+  providedIn: 'root',
 })
 export class ChatService {
-	private backend = environment.backend + "api/"
+  private backend = environment.backend + 'api/';
 
-	constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-	private get getUser(): any {
-		return JSON.parse(localStorage.getItem("user")!)
-	}
+  private get getUser(): any {
+    return JSON.parse(localStorage.getItem('user')!);
+  }
 
-	private get token(): any {
-		return JSON.parse(localStorage.getItem("token")!)
-	}
+  private get token(): any {
+    return JSON.parse(localStorage.getItem('token')!);
+  }
 
-	async readChats() {
-		const url = this.backend + "chat/readChats"
-		return this.http.get(url, {
-			headers: { "x-access-token": this.token },
-		})
-	}
-	async sendMessage(body: any) {
-		const url = this.backend + "chat/sendMessage"
-		return this.http.post(url, body, {
-			headers: { "x-access-token": this.token },
-		})
-	}
+  async readChats() {
+    const url = this.backend + 'chat/readChats';
+    return this.http.get(url, {
+      headers: { 'x-access-token': this.token },
+    });
+  }
+  async sendMessage(body: any) {
+    const url = this.backend + 'chat/sendMessage';
+    return this.http.post(url, body, {
+      headers: { 'x-access-token': this.token },
+    });
+  }
 
-	async readMessages(body: any) {
-		const url = this.backend + "chat/getChatByIdUser"
-		return this.http.post(url, body, {
-			headers: { "x-access-token": this.token },
-		})
-	}
-	async updateMessage(body: any) {
-		const url = this.backend + "chat/updateMessage"
-		return this.http.put(url, body, {
-			headers: { "x-access-token": this.token },
-		})
-	}
+  async readMessages(body: any) {
+    const url = this.backend + 'chat/getChatByIdUser';
+    return this.http.post(url, body, {
+      headers: { 'x-access-token': this.token },
+    });
+  }
+  async updateMessage(body: any) {
+    const url = this.backend + 'chat/updateMessage';
+    return this.http.put(url, body, {
+      headers: { 'x-access-token': this.token },
+    });
+  }
+
+  async getLastMessage(remitente_id: number, destinatario_id: number) {
+    const url =
+      this.backend + `chat/getLastMessage/${remitente_id}/${destinatario_id}`;
+    return this.http
+      .get(url, {
+        headers: { 'x-access-token': this.token },
+      })
+      .toPromise();
+  }
 }
